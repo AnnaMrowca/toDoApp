04c3f091f1c509ec9699ce8f1caec8da57f61e92
AnnaMrowca <amrowca@gmail.com>
2021-05-17 22:56:07 +0200

    updating application in settings.py, creating task and adding user in models

--- toDo/models.py
+++ toDo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+#automatyczny model Usera w Django, nie trzeba dodawać go jako klasy, chyba że chcemy usera modyfikować =
+# = jeśli chcemy by user logowł się przez email lub dodać jakieś pole itd.
 
-# Create your models here.
+# tytul
+# opis(opcjonalny)
+# wazne(lub nie)
+# data utworzenia (ktora ma sie dodawac automatycznie)
+# data zakonczenia (dodawane recznie)
+# polacznie z userem
+
+class Task(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField(blank=True)
+    importance = models.BooleanField(default=False)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    completion_date = models.DateTimeField(blank=True, null=True)#większość pól z wyjątkiem charfield i textfield wymagają blank i null = True
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #on_delete=models.CASCADE = wpis zniknie, jeśli usuniemy usera
+
+
