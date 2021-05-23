387278fc9c7a63b3932af95ad7cd62916004298a
AnnaMrowca <amrowca@gmail.com>
2021-05-23 20:55:06 +0200

    adding password validation details: saving if passwords match  and returning 'no match' message if passwords are not the same

--- toDo/views.py
+++ toDo/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 def register(request):
     if request.method == "GET":
         return render(request, "register.html",{'form':UserCreationForm()})
+    else:
+        if request.POST.get('password1') == request.POST.get('password2'):
+            user =User.objects.create_user(username=request.POST.get(
+                'username'), password=request.POST.get('password2'))
+            user.save()
+            return redirect('register')
+        else:
+            error = "Your passwords do not match"
+            return render(request, 'register.html', {'form': UserCreationForm(), 'error': error})
+
+
+
+
